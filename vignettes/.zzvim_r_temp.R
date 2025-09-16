8002b85291e6a2d77b8ae49f42dc8ffd6d9bb437
r. glenn thomas <rgthomas@ucsd.edu>
2025-09-15 19:30:37 -0700

    Auto-backup: 2025-09-15 19:30:37

--- vignettes/.zzvim_r_temp.R
+++ vignettes/.zzvim_r_temp.R
@@ -1,1 +1,20 @@
-install.packages("zztab2fig")
+# Create a table with custom margins and language support
+t2f(
+  df = mtcars,
+  filename = "mtcars_custom",
+  sub_dir = "tables", 
+  extra_packages = list(
+    geometry(margin = "5mm", paper = "a4paper"),
+    babel("spanish")
+  ),
+  verbose = TRUE
+)
+
+# Wide table with landscape orientation
+t2f(
+  df = mtcars,
+  filename = "mtcars_wide",
+  extra_packages = list(
+    geometry(margin = "3mm", landscape = TRUE)
+  )
+)
